21d85f6d1b02a5648d7a8d289b1c95a369b74be5
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2022-04-28 15:11:15 -0500

    Commit

---
+++ DataPostgres.py
@@ -0,0 +1,1 @@
+# key: ekP7ukk8wMIDNKmE
--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/students'
+
 @app.route('/')
 def dash():
     return render_template('index.html')
